8fe17c05630ad85a93a5ba28945be24805f79da4
Moncef Betaieb <moncefbettaieb@gmail.com>
2025-03-22 17:39:02 +0100

    improve dbt models

--- models/staging/curated/stg_unified_images.sql
+++ models/staging/curated/stg_unified_images.sql
@@ -9,7 +9,7 @@ WITH gdd AS (
         source,
         image_links,
         processed_time
-    FROM {{ source('pharma_sources', 'raw_pharma_gdd') }}
+    FROM {{ ref('snapshot_pharma_gdd') }}
     WHERE image_links IS NOT NULL
 ),
 
@@ -19,7 +19,7 @@ centre AS (
         source,
         image_src  AS image_url,
         processed_time
-    FROM {{ source('pharma_sources', 'raw_pharmacie_du_centre') }}
+    FROM {{ ref('snapshot_pharma_centre') }}
     WHERE image_src IS NOT NULL
     AND image_src LIKE '%' || source || '%'
 ),
@@ -37,8 +37,7 @@ pharma_gdd_images AS (
 SELECT DISTINCT
     cip_code,
     source,
-    image_url,
-    CURRENT_TIMESTAMP AS last_update
+    image_url
 FROM (
     SELECT * FROM pharma_gdd_images
     UNION ALL
