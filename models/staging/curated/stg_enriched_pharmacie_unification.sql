4ca96d9bc1fc41c53c7a72a42b757c882b552202
Moncef Betaieb <moncefbettaieb@gmail.com>
2025-03-27 17:22:20 +0100

    improve dbt models

--- models/staging/curated/stg_enriched_pharmacie_unification.sql
+++ models/staging/curated/stg_enriched_pharmacie_unification.sql
@@ -15,8 +15,4 @@
         dim_ct.taxonomy_sub_category3
     FROM {{ ref('stg_pharmacie_unification') }} unif
     LEFT JOIN {{ ref('dim_match_categorie_taxonomy') }} dim_ct
-    ON  unif.categorie = dim_ct.categorie
-    AND unif.sous_categorie_1 = dim_ct.sous_categorie_1
-    AND unif.sous_categorie_2 = dim_ct.sous_categorie_2
-    AND unif.sous_categorie_3 = dim_ct.sous_categorie_3
-    AND unif.combined_categorie = dim_ct.combined_categorie
+    ON  unif.combined_categorie = dim_ct.combined_categorie
